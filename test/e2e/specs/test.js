be8e5819fa526a77b1c5af42a28a8ff57d53a8f4
unknown <force-special@hotmail.com>
2018-05-18 10:39:09 +0000

    adding e2e test

---
+++ test/e2e/specs/test.js
@@ -0,0 +1,20 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'default e2e tests': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.card')
+      .pause(1000)
+      .click('button[type=submit]')
+      .waitForElementVisible('.snack__wrapper', 3000)
+      .end()
+  }
+}
