98f03f4ed4f59037ba3912281cb03b3e44a9d293
unknown <force-special@hotmail.com>
2018-06-01 14:23:13 +0000

    testing ci/cd

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ import store  from '../store/index'
 let baseURL
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    // baseURL = 'http://localhost:1337'
     baseURL = 'http://app-sails:1337'
 } else if (process.env.NODE_ENV === 'production') {
     baseURL = 'http://backend.tech.sdrm.me:7331'
